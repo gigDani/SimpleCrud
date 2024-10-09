90a1cdfa8f3e37b0dd7b6523b1015e0d2f5c17da
Даниил <113928056+gigDani@users.noreply.github.com>
2024-10-09 10:57:38 +0300

    Start

---
+++ SimpleCrud/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Crud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Сервис работает.");
+        }
+    }
+}
---
+++ SimpleCrud/Controllers/ProductsController.cs
@@ -0,0 +1,67 @@
+﻿using SimpleCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> Products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Гуашь", Price = 50.0m },
+            new Product { Id = 2, Name = "Гитара", Price = 150.0m }
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return Products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult<Product> Post(Product product)
+        {
+            product.Id = Products.Max(p => p.Id) + 1;
+            Products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Product product)
+        {
+            var existingProduct = Products.FirstOrDefault(p => p.Id == id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            existingProduct.Name = product.Name;
+            existingProduct.Price = product.Price;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product = Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            Products.Remove(product);
+            return NoContent();
+        }
+    }
+}
+
